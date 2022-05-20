cb8b4018b1644eb1b1b6a32da83d2890078e1313
kochubey-kseniya <96043439+kochubey-kseniya@users.noreply.github.com>
2022-05-20 19:10:35 +0300

    feat: update index.js

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,17 +1,20 @@
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
+const { stdin: input, stdout: output } = require('process');
+
 const stream = fs.createWriteStream(path.join(__dirname,'text.txt'));
 const rl = readline.createInterface({ input, output });
 
-rl.write('Введите ваш текст, пожалуйста!\n');
-rl.on('data', data => {
-if (data === 'exit') {
-    rl.write('Программа будет закрыта');
+rl.write('Введите ваш текст, пожалуйста\n');
+rl.on('line', data => {
+  if (data === 'exit') {
+    rl.write('Программа будет закрыта\n');
     process.exit();
-    }
+  }
   stream.write(`${data}\n`);
 });
 rl.on('close', () => {
-  rl.write('Программа будет закрыта');
+  rl.write('Программа будет закрыта\n');
   process.exit();
 });
